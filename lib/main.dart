bdb2eb51794f9c4608d26e9971f59883f6523982
matthew colson <matthew.p.colson@gmail.com>
2021-01-11 11:52:42 -0600

    added key that play different notes when pressed.

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,40 @@
+import 'package:audioplayers/audio_cache.dart';
 import 'package:flutter/material.dart';
 
 void main() => runApp(XylophoneApp());
 
 class XylophoneApp extends StatelessWidget {
+  final audioCache = new AudioCache();
+
+  Widget buildKey(int soundNumber, Color color) {
+    return Expanded(
+      child: FlatButton(
+          color: color,
+          onPressed: () {
+            audioCache.play('note$soundNumber.wav');
+          },
+          child: null),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
+        backgroundColor: Colors.black,
         body: SafeArea(
-          child: Container(),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: [
+              buildKey(1, Colors.red),
+              buildKey(2, Colors.orange),
+              buildKey(3, Colors.yellow),
+              buildKey(4, Colors.green),
+              buildKey(5, Colors.teal),
+              buildKey(6, Colors.blue),
+              buildKey(7, Colors.purple),
+            ],
+          ),
         ),
       ),
     );
